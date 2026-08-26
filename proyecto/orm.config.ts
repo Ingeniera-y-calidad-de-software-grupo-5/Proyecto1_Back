0ebe5e2fa252d9fabfb71b3b81d3180126257888
MaschioPablo <maschiopablo5@gmail.com>
2026-08-26 19:30:23 -0300

    agregar configuración SSL para la conexión a la base de datos

--- proyecto/orm.config.ts
+++ proyecto/orm.config.ts
@@ -14,6 +14,7 @@ export default new DataSource({
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
 
   entities: [__dirname + '/src/**/*.entity.ts'],
   migrations: [__dirname + '/src/migrations/*{.ts,.js}'],
